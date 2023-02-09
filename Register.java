772bd4443ebed10e6e2879fa675585a95d672d1f
Qydnama <123525806+Qydnama@users.noreply.github.com>
2023-02-09 22:41:52 +0600

    Update Register.java

--- Register.java
+++ Register.java
@@ -11,10 +11,8 @@ public class Register {
 
     public boolean register() {
         try {
-            // Connect to the database
             Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "raimbek99");
 
-            // Check if the username already exists in the database
             PreparedStatement checkUsernameStatement = connection.prepareStatement("SELECT username FROM users WHERE username = ?");
             checkUsernameStatement.setString(1, username);
             ResultSet resultSet = checkUsernameStatement.executeQuery();
@@ -22,7 +20,6 @@ public class Register {
                 return false;
             }
 
-            // Insert the new user into the database
             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (username, password) VALUES (?, ?)");
             preparedStatement.setString(1, username);
             preparedStatement.setString(2, password);
